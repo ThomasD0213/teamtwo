b08dc9c3670cfdc5e454ed9c11290cd623e027b8
Sariah Shoemaker <sariah.shoemaker@du.edu>
2023-10-21 20:19:24 -0600

    Added project page components

--- front-end/src/app/projectPage/page.tsx
+++ front-end/src/app/projectPage/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import {Form, FormGroup, FormLabel, FormControl, FormText} from "../../../node_modules/react-bootstrap/esm/index";
-import Button from '../../../node_modules/react-bootstrap/esm/Button';
+import {Badge, Button, Stack} from "../../../node_modules/react-bootstrap/esm/index";
 
 const projectPage = () => {
   return (
@@ -9,14 +8,15 @@ const projectPage = () => {
             Name of Project
         </h1>
 
-        <Form>
-            <FormGroup>
-                <FormLabel></FormLabel>
-                <FormControl></FormControl>
-                <FormText></FormText>
-            </FormGroup>
-        </Form>
-
+        <Stack direction="vertical">
+            <Badge bg="primary">OPEN/CLOSED</Badge>
+            <text><b>Owner:</b> John Smith {"\n"}</text>
+            <text><b>Details:</b> Lorem ipsum {"\n"}</text>
+            <text><b>Skills Needed:</b> Awesome react skills {"\n"}</text>
+            <text><b>Project Timeline:</b> Finish by Nov 23 2023 {"\n"}</text>
+            <text><b>Budget:</b> N/A {"\n"}</text>
+            <text><b>Contact:</b> John.Smith@du.edu {"\n"}</text>
+        </Stack>
     </div>
   )
 }
