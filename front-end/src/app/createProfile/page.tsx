f8faa20c38f19a7892cbefd0ca943fae937670bd
Sariah Shoemaker <sariah.shoemaker@du.edu>
2023-10-21 20:52:31 -0600

    Added create profile survey

--- front-end/src/app/createProfile/page.tsx
+++ front-end/src/app/createProfile/page.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+'use client';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export const CreateProfile = () => {
+function createProfile() {
   return (
-    <div>
-        <h1>Create Profile</h1>
-    </div>
-  )
+    <Form>
+      <Form.Group className="mb-3" controlId="Name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control placeholder="Enter name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="Role">
+        <Form.Label>Role</Form.Label>
+        <Form.Select>
+            <option></option>
+            <option>Student</option>
+            <option>Professor</option>
+        </Form.Select>.
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="Study">
+        <Form.Label>Major / Field</Form.Label>
+        <Form.Control placeholder="Enter major or field" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="Bio">
+        <Form.Label>Bio</Form.Label>
+        <Form.Control placeholder="Enter a little about yourself" />       
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="Contact">
+        <Form.Label>Best Form of Contact</Form.Label>
+        <Form.Control placeholder="Enter email address or phone number" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
-export default CreateProfile
+export default createProfile;
