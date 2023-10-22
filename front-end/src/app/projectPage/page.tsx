8952aacffc13c0b61f4e395d926bf17c9b3172ba
Sariah Shoemaker <sariah.shoemaker@du.edu>
2023-10-21 23:11:31 -0600

    added basic grid

--- front-end/src/app/projectPage/page.tsx
+++ front-end/src/app/projectPage/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react'
-import {Badge, Stack, Button, Container} from "../../../node_modules/react-bootstrap/esm/index";
+import {Badge, Stack, Button, Container, Row, Col} from "../../../node_modules/react-bootstrap/esm/index";
 import './project.css';
 import Image from 'next/image';
 import arrow from './images/leftArrow.png';
@@ -16,7 +16,7 @@ const projectPage = () => {
         /></Button>
     </div>
     <div>
-        <h1>
+        {/* <h1>
             Name of Project
         </h1>
 
@@ -28,7 +28,28 @@ const projectPage = () => {
             <text><b>Project Timeline:</b> Finish by Nov 23 2023 {"\n"}</text>
             <text><b>Budget:</b> N/A {"\n"}</text>
             <text><b>Contact:</b> John.Smith@du.edu {"\n"}</text>
-        </Stack>
+        </Stack> */}
+
+        <Container fluid>
+            <Row>
+                <Col id = "TopRectangle">PROJECT TITLE</Col>
+                <Col id = "Contact">Contact Me: John.Smith@university.edu</Col>
+            </Row>
+            <Row>
+                <Col id = "BottomRectangle">
+                    <Stack>
+                        <p>OPEN/CLOSED</p>
+                        <p><b>Owner</b></p>
+                        <p>John Smith</p>
+                        <p><b>Skills Needed</b></p>
+                        <p>Mad react skills</p>
+                        <p><b>Project Timeline</b></p>
+                        <p>Finish by Nov 23 2023</p>
+                    </Stack>
+                </Col>
+                <Col id = "Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</Col>
+            </Row>
+        </Container>
     </div>
     </main>
   )
