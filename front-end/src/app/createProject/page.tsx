5aedfe7c8494666b5c1e8b9bda52f3b03e77674f
Sariah Shoemaker <sariah.shoemaker@du.edu>
2023-10-22 12:48:24 -0600

    Formatted create project profile

--- front-end/src/app/createProject/page.tsx
+++ front-end/src/app/createProject/page.tsx
@@ -8,6 +8,7 @@ import InputGroup from '../../../node_modules/react-bootstrap/esm/InputGroup';
 import Row from '../../../node_modules/react-bootstrap/esm/Row';
 import React from 'react';
 import { TagsInput } from "../../../node_modules/react-tag-input-component/dist/esm/index";
+import { Container } from 'react-bootstrap';
 
 function FormExample() {
   const [validated, setValidated] = useState(false);
@@ -72,84 +73,116 @@ function FormExample() {
   }
 
   return (
+    <main>
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01" onChange={(e:any) => setName(e.target.value)}>
-          <Form.Label>Project Title</Form.Label> {/* text box title */}
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter your project title" // when there is nothing typed in, this is shown in the textbox
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom02" onChange={(e:any) => setDescription(e.target.value)}>
-          <Form.Label>Details</Form.Label>
-          <Form.Control
-            as="textarea"
-            required
-            rows={8}
-            type="text"
-            placeholder="Enter project details"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom05" onChange={(e:any) => setSkills(e.target.value)}>
-          <Form.Label>Skills</Form.Label>
-          <TagsInput
-            value={selected}
-            onChange={setSelected}
-            name="skills"
-            placeHolder="Add a skill and press 'Enter'"
-            onBlur={undefined} 
-            separators={undefined} 
-            disableBackspaceRemove={undefined} 
-            onExisting={undefined} 
-            onRemoved={undefined} 
-            disabled={undefined} 
-            isEditOnRemove={undefined} 
-            beforeAddValidate={undefined} 
-            onKeyUp={undefined} 
-            classNames={undefined}            />          
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="2" controlId="validationCustom04" onChange={(e:any) => setPaid(e.target.value)}>
-          <Form.Label>Budget</Form.Label>  {/** set to Paid and change to drop down with options yes or no */}
-          <InputGroup className="mb-3">
-            <InputGroup.Text>$</InputGroup.Text>
-            <Form.Control type="text" placeholder="Enter your budget" required />
-            <InputGroup.Text>.00</InputGroup.Text>
-          </InputGroup>
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid number.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="3" controlId="validationCustom04" onChange={(e:any) => setTimeline(e.target.value)}>
-          <Form.Label>Timeline</Form.Label>
-          <Form.Control type="text" placeholder="Enter your timeline" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid time frame.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="3" controlId="validationCustom04" onChange={(e:any) => setContact(e.target.value)}>
-          <Form.Label>Contact</Form.Label>
-          <Form.Control type="text" placeholder="Enter your email or phone number" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide valid contact information.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Button type="submit">Submit</Button>
+      <Container>
+        <Row>
+          <Col>        
+            <Form.Group as={Col} md="4" controlId="validationCustom01" onChange={(e:any) => setName(e.target.value)}>
+            <Form.Label>Project Title</Form.Label> {/* text box title */}
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter your project title" // when there is nothing typed in, this is shown in the textbox
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group as={Col} md="4" controlId="validationCustom05" onChange={(e:any) => setSkills(e.target.value)}>
+            <Form.Label>Skills</Form.Label>
+            <TagsInput
+              value={selected}
+              onChange={setSelected}
+              name="skills"
+              placeHolder="Add a skill and press 'Enter'"
+              onBlur={undefined} 
+              separators={undefined} 
+              disableBackspaceRemove={undefined} 
+              onExisting={undefined} 
+              onRemoved={undefined} 
+              disabled={undefined} 
+              isEditOnRemove={undefined} 
+              beforeAddValidate={undefined} 
+              onKeyUp={undefined} 
+              classNames={undefined}            />          
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Application Deadline</Form.Label>
+              <Form.Control type="text" placeholder="Enter the application deadline" />
+            </Form.Group>
+          </Col>
+
+          <Col>
+            <Form.Group as={Col} md="3" controlId="validationCustom04" onChange={(e:any) => setTimeline(e.target.value)}>
+            <Form.Label>Timeline</Form.Label>
+            <Form.Control type="text" placeholder="Enter your timeline" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid time frame.
+            </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+
+          <Col>
+            <Form.Group as={Col} md="2" controlId="validationCustom04" onChange={(e:any) => setPaid(e.target.value)}>
+            <Form.Label>Paid/Unpaid</Form.Label>  {/** set to Paid and change to drop down with options yes or no */}
+            <Form.Select>
+              <option></option>
+              <option>Paid</option>
+              <option>Unpaid</option>
+            </Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Form.Group as={Col} md="4" controlId="validationCustom02" onChange={(e:any) => setDescription(e.target.value)}>
+            <Form.Label>Details</Form.Label>
+            <Form.Control
+              as="textarea"
+              required
+              rows={8}
+              type="text"
+              placeholder="Enter project details"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+
+          </Col>
+
+          <Col>
+            <Form.Group>
+                <Form.Label>Important Links</Form.Label>
+                <Form.Control type="text" placeholder="Place any important links here"/>
+              </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+              <Form.Group as={Col} md="3" controlId="validationCustom04" onChange={(e:any) => setContact(e.target.value)}>
+              <Form.Label>Contact</Form.Label>
+              <Form.Control type="text" placeholder="Enter your email or phone number" required />
+              <Form.Control.Feedback type="invalid">
+                Please provide valid contact information.
+              </Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+
+            <Col>
+              <Button type="submit">Submit</Button>
+            </Col>
+        </Row>
+      </Container>
+     
     </Form>
+    </main>
   );
 }
 
