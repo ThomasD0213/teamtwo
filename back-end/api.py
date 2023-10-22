23c5f115e0753406907e1b9342f222f8fce55987
Thomas Dalgetty <tdalgetty0213@gmail.com>
2023-10-22 02:10:19 -0600

    made some changes to createProfile

--- back-end/api.py
+++ back-end/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import sqlite3
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
--- front-end/src/app/createProfile/page.tsx
+++ front-end/src/app/createProfile/page.tsx
@@ -1,20 +1,46 @@
 'use client';
 // import Button from 'react-bootstrap/Button';
 // import Form from 'react-bootstrap/Form';
+import {FormEvent, useState} from 'react'
 import {Form, Button, Navbar, Container, Nav, NavDropdown} from "../../../node_modules/react-bootstrap/esm/index";
 import Bar from '../../../Components/bar'
 
 function createProfile() {
+
+  const formStuff = () => { //TODO: get this to work
+    const [formData, setFormData] = useState({
+      first_name: "",
+      last_name: "",
+      role: "",
+      major: "",
+      bio: "",
+      email: "",
+      phone: ""
+    })
+  }
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    console.log(Array.from(formData))
+    const response = await fetch('http://127.0.0.1:5000/addUser', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+    })
+  }
+//const handleChange = (event: any) => { //TODO: get this to work for OnChange with the form
+//      setFormData({[e.target.name]: event.target.value})
+//  }
   return (
     <main>
       <Bar />
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="First_Name">
           <Form.Label>First Name</Form.Label>
           <Form.Control required placeholder="Enter first name" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="Last_Name">
+        <Form.Group className="mb-3" controlId="Last_Name"> 
           <Form.Label>Last Name</Form.Label>
           <Form.Control required placeholder="Enter last name" />
         </Form.Group>
@@ -25,17 +51,17 @@ function createProfile() {
               <option>Student</option>
               <option>Professor</option>
           </Form.Select>
-          <Form.Control required />
-
+          <Form.Control type='radio' required />
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="Study">
           <Form.Label>Major / Field</Form.Label>
-          <Form.Control placeholder="Enter major or field" />
+          <Form.Control required placeholder="Enter major or field" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Bio">
           <Form.Label>Bio</Form.Label>
-          <Form.Control placeholder="Enter a little about yourself" />       
+          <Form.Control required placeholder="Enter a little about yourself" />       
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Email">
@@ -47,7 +73,7 @@ function createProfile() {
         </Form.Group>
         
         <Form.Group className="mb-3" controlId="Phone">
-          <Form.Label>Email</Form.Label>
+          <Form.Label>Phone Number</Form.Label>
           <Form.Control placeholder="Enter phone number" />
           <Form.Text className="text-muted">
             We'll never share your number with anyone else.
